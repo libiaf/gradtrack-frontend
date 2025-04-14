8fa1013738ad9a11433ebfd11a0a3dac0afed964
dianaescg <a01253468@tec.mx>
2025-04-14 01:48:17 -0700

    Crear dashboard

--- gradtrack-front/src/api/EvaluadoAPI.ts
+++ gradtrack-front/src/api/EvaluadoAPI.ts
@@ -5,7 +5,6 @@ import { Evaluado } from "my-types";
 export const getAllEvaluados = async () => {
     try {
         const res = await api.get(`/evaluados/getallevaluados`);
-        // console.log(res.data); -> for connection testing purpose
         const evaluados: Evaluado[] = await res.data.payload;
         return evaluados;
     } catch (err) {
@@ -44,6 +43,58 @@ export const getPoblacionesByZona = async (zonaId: number) => {
   } catch (error) {
       console.error("Error al obtener poblaciones:", error);
       return [];
+  }
+};
+
+// PARA DASHBOARD
+
+export const getTotalEvaluados = async (poblacionId: string) => {
+  try {
+    const res = await api.get(`/evaluados/totalEvaluados/${poblacionId}`);
+    return res.data;
+  } catch (error) {
+    console.error('Error al obtener total de evaluados:', error);
+    throw error;
+  }
+};
+
+export const getPorcentajeGraduacion = async (poblacionId: string) => {
+  try {
+    const res = await api.get(`/evaluados/graduacion/${poblacionId}`);
+    return res.data;
+  } catch (error) {
+    console.error('Error al obtener porcentaje de graduación:', error);
+    throw error;
+  }
+};
+
+export const getGraduacionHombres = async (poblacionId: string) => {
+  try {
+    const res = await api.get(`/evaluados/graduacion/hombres/${poblacionId}`);
+    return res.data;
+  } catch (error) {
+    console.error('Error al obtener graduación de hombres:', error);
+    throw error;
+  }
+};
+
+export const getGraduacionMujeres = async (poblacionId: string) => {
+  try {
+    const res = await api.get(`/evaluados/graduacion/mujeres/${poblacionId}`);
+    return res.data;
+  } catch (error) {
+    console.error('Error al obtener graduación de mujeres:', error);
+    throw error;
+  }
+};
+
+export const getNivelAlerta = async (poblacionId: string) => {
+  try {
+    const res = await api.get(`/evaluados/nivelAlerta/${poblacionId}`);
+    return res.data;
+  } catch (error) {
+    console.error('Error al obtener nivel de alerta:', error);
+    throw error;
   }
 };
   

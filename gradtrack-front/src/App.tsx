2f0dbdaf6056e980287b01c68e2276c4acc62cec
libiaf <a01253425@tec.mx>
2025-04-13 20:55:42 -0700

    Crear Header

--- gradtrack-front/src/App.tsx
+++ gradtrack-front/src/App.tsx
@@ -4,7 +4,6 @@ const App = () => {
   return (
     <div className="d-flex flex-column flex-lg-row min-vh-100">
       <div className="flex-grow-1">
-        <h1>Hello world</h1>
         <Outlet />
       </div>
     </div>
